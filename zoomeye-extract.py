c81543c957c5e756a94930cb5ce432cf4354ab52
arpandaze <46302068+arpandaze@users.noreply.github.com>
2019-01-04 19:17:13 +0545

    Added feature to scrape port too.

--- zoomeye-extract.py
+++ zoomeye-extract.py
@@ -6,8 +6,8 @@ import urllib.parse
 import pickle
 
 # This a change station. You can change link filename and everything here.
-scrapingOutputFileName = "links"  # Enter the name of file to save scraped IPs.
-searchTerm = '' #Enter your search term here. You must enter in exactly same pattern as in ZoomEye website.
+scrapingOutputFileName = "scrapedIPs"  # Enter the name of file to save scraped IPs.
+searchTerm = 'searchItem' #Enter your search term here. You must enter in exactly same pattern as in ZoomEye website.
 cookieFileName = 'cookie' #Name of the cookie file.
 pageLoadoutTime = 60  # This is time to wait for page to load.
 totalNumberOfPages = 20 # Number of pages to scrape from.
@@ -40,29 +40,42 @@ while True :
 #Defining the ways to surf the webpage.
 def pageExplorer():
     file = open(scrapingOutputFileName + ".txt", "a")
+    portCollection = []
     clickLinks = browser.find_elements_by_xpath(".//a[contains(@class,'search-result-item-title')]")
-    for links in clickLinks :
+    portFinder = browser.find_elements_by_xpath(".//button[contains(@class,'ant-btn ant-btn-primary')]")
+    for ports in portFinder:
+        getPortInnerHTML = ports.get_attribute('innerText')
+        try :
+            portCollection = portCollection + [str(getPortInnerHTML[0:getPortInnerHTML.index('/')])]
+        except ValueError:
+            portCollection = portCollection + ['']
+    loopCounterToMatchPortWithIP = 0
+    for links in clickLinks:
         ip = links.get_attribute("innerHTML")
         if ip[0:2] != '<i':
-            file.write(ip+"\n")
-            print(ip)
-    browser.find_element_by_class_name("ant-pagination-next").click()
-    time.sleep(5)
+            if portCollection[loopCounterToMatchPortWithIP] == '' :
+                file.write(ip + "\n")
+            else :
+                file.write(ip + ':' + portCollection[loopCounterToMatchPortWithIP] + "\n")
+            loopCounterToMatchPortWithIP += 1
+    browser.find_element_by_xpath(".//li[contains(@class,'ant-pagination-next')]").click()
+    while len(browser.find_elements_by_xpath(".//li[contains(@class,'ant-pagination-next')]"))==0 :
+        time.sleep(1)
 
 #Loading the ZoomEye Search page.
 browser.get(zoomeye+ '/searchResult?q=' + urllib.parse.quote(searchTerm))
 
 #Checking if the page has loaded. If not wait for 5 secs.
 while len(browser.find_elements_by_xpath(".//i[contains(@class,'anticon anticon-right-circle')]/ancestor::a"))==0 :
-    time.sleep(5)
-    print('Waiting 5 more seconds!')
+    time.sleep(1)
+    print('Waiting 1 more seconds!')
 
 currentPageNumber = 1
 while currentPageNumber <= totalNumberOfPages :
     pageExplorer()
     print('Page '+str(currentPageNumber)+' done!')
     currentPageNumber += 1
-    if currentPageNumber == totalNumberOfPages + 1 :
+    if currentPageNumber > totalNumberOfPages :
         print('All done!')
         browser.quit()
         exit()
